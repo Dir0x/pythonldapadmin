3cd3e1d8b24e27a952d3725479ac0aca45b764f9
Daniel <daniel@debian>
2022-05-21 03:18:16 +0200

    Create childs fixed and create groups

--- app/routes.py
+++ app/routes.py
@@ -5,15 +5,8 @@ from passlib.hash import ldap_md5_crypt
 import ldap.modlist as modlist
 
 # To do
-# Crear sección de ajustes para definir variables como directorio de usuarios
-# OU por defecto en ajustes
-# modo no redirección en los ajustes para que te mande al home o no al añadir usuarios
-#
-# DONE Modificar usuarios
-# Añadir grupos
-# Eliminar grupos
-# Modificar grupos
-# Mover usuarios entre grupos
+# Mover usuarios entre OUs
+# Añadir OUs
 
 @app.route('/login',methods=['GET', 'POST'])
 def login():
@@ -113,48 +106,90 @@ def modify_user(name):
 
 	return render_template("modify_user.html", value=value)
 
-@app.route('/add_user', methods=['GET', 'POST'])
-def add_user():
-	if request.method == "GET":
-		return render_template('add_user.html')
-	elif request.method == "POST":
-		fullname = request.form['username']
-		home_dir = request.form['home']
-		gid = request.form['gid']
-		shell = request.form['shell']
-		password1 = request.form['password1']
-		password2 = request.form['password2']
-		uid = request.form['uid']
-		if password1 != password2:
-			flash("Las contraseñas deben coincidir")
-			return redirect(url_for('add_user'))
-		hash = ldap_md5_crypt.hash(password1)
-		entry = []
-		entry.extend([
-			('objectClass', [b"inetOrgPerson", b"organizationalPerson", b"posixAccount", b"person"]),
-			('uid', bytes(uid, 'UTF-8')),
-			('sn', bytes(fullname, 'UTF-8')),
-			('uidNumber', b"2000"),
-			('gidNumber', bytes(gid, 'UTF-8')),
-			('loginShell', bytes(shell, 'UTF-8')),
-			('homeDirectory', bytes(home_dir, 'UTF-8')),
-			('userPassword', bytes(hash, 'UTF-8'))
-		])
-		try:
-			try:
-				l = ldap.initialize("ldap://" + session['address'])
-				bind = l.simple_bind_s("cn=" + session['user'] + "," + session['domain'], session['password'])
-			except:
-				flash("Can't connect to LDAP server")
-				return redirect(url_for('add_user'))
+@app.route('/create_object')
+def create_object():
+	return render_template("create_object.html")
 
-			l.add_s("cn=" + fullname + ",dc=dharo,dc=local", entry)
-			l.unbind_s()
-			flash("User added")
-			return redirect(url_for('home'))
-		except:
-			flash("An error occurred")
-			return redirect(url_for('add_user'))
+@app.route('/add_user', methods=['POST'])
+def add_user():
+	fullname = request.form['username']
+	home_dir = request.form['home']
+	gid = request.form['gid']
+	shell = request.form['shell']
+	password1 = request.form['password1']
+	password2 = request.form['password2']
+	uid = request.form['uid']
+
+	if password1 != password2:
+		flash("Las contraseñas deben coincidir")
+		return redirect(url_for('create_object'))
+
+	hash = ldap_md5_crypt.hash(password1)
+	entry = []
+	entry.extend([
+		('objectClass', [b"inetOrgPerson", b"organizationalPerson", b"posixAccount", b"person"]),
+		('uid', bytes(uid, 'UTF-8')),
+		('sn', bytes(fullname, 'UTF-8')),
+		('uidNumber', b"2000"),
+		('gidNumber', bytes(gid, 'UTF-8')),
+		('loginShell', bytes(shell, 'UTF-8')),
+		('homeDirectory', bytes(home_dir, 'UTF-8')),
+		('userPassword', bytes(hash, 'UTF-8'))
+	])
+
+	try:
+		l = ldap.initialize("ldap://" + session['address'])
+		bind = l.simple_bind_s("cn=" + session['user'] + "," + session['domain'], session['password'])
+	except:
+		flash("Can't connect to LDAP server")
+		return redirect(url_for('create_object'))
+
+	if 'parent' in request.form:
+		parent = request.form['parent']
+
+	else:
+		parent = session['domain']
+
+	try:
+		l.add_s("cn=" + fullname + "," + parent, entry)
+		l.unbind_s()
+		flash("User added")
+		return redirect(url_for('home'))
+
+	except:
+		flash("Couldn't create the user")
+		return redirect(url_for('home'))
+
+@app.route("/add_group", methods=['POST'])
+def add_group():
+	try:
+		l = ldap.initialize("ldap://" + session['address'])
+		bind = l.simple_bind_s("cn=" + session['user'] + "," + session['domain'], session['password'])
+	except:
+		flash("Can't connect to LDAP server")
+		return redirect(url_for('home'))
+		
+	if 'parent' in request.form:
+		parent = request.form['parent']
+
+	else:
+		parent = session['domain']
+		
+	entry = [('objectClass', [b'posixGroup', b'top']), 
+				('cn', [bytes(request.form['cn'], 'UTF-8')]), 
+				('gidNumber', [bytes(request.form['gid'], 'UTF-8')])]
+
+	try:
+		l.add_s("cn=" + request.form['cn'] + "," + parent ,entry)
+		l.unbind_s()
+		flash("Group created")
+		return redirect(url_for('home'))
+
+	except:
+		flash("An error occurred")
+		return redirect(url_for('home'))
+
+	return parent
 
 @app.route("/delete_user", methods=['POST'])
 def delete_user():
@@ -165,12 +200,18 @@ def delete_user():
 				bind = l.simple_bind_s("cn=" + session['user'] + "," + session['domain'], session['password'])
 			except:
 				flash("Can't connect to LDAP server")
+				return redirect(url_for('home'))
 
-			l.delete(request.form['user']) # TBD revisar esto que no funca. añadir el dominio creo
-			l.unbind_s()
-			flash("User deleted")
-			time.sleep(3)
-			return redirect(url_for('home'))
+			try:
+				l.delete(request.form['user'])
+				l.unbind_s()
+				flash("User deleted")
+				time.sleep(1)
+				return redirect(url_for('home'))
+			except:
+				flash("Unable to delete the user")
+				return redirect(url_for('home'))
+
 		except:
 			flash("An error occurred")
 			return redirect(url_for('delete_user'))
@@ -194,6 +235,7 @@ def home():
 		results = sorted(new_results)
 
 		last = 0
+		parents = []
 		tags = []
 
 		for result in results:
@@ -210,33 +252,43 @@ def home():
 				printable = result[len(result)-1]
 
 			if printable.startswith('cn='):
-				printable += " <a href='modify_user/" + inverted + "'>Modify" '</a><form action="/delete_user" method="POST">\n	<input type="hidden" name="user" value="' + printable + '">\n    <input type="image" src="https://cdn-icons-png.flaticon.com/512/58/58326.png" alt="submit" width="20" height="20">\n</form>'
+				printable += " <a href='modify_user/" + inverted + "'>Modify" '</a><form action="/delete_user" method="POST">\n	<input type="hidden" name="user" value="' + inverted + '">\n    <input type="image" src="https://cdn-icons-png.flaticon.com/512/58/58326.png" alt="submit" width="20" height="20">\n</form>'
 			else:
-				printable += " <a href='modify_user/" + inverted + "'>Modify" '</a><form action="/delete_user" method="POST">\n	<input type="hidden" name="user" value="' + printable + '">\n    <input type="image" src="https://cdn-icons-png.flaticon.com/512/58/58326.png" alt="submit" width="20" height="20">\n</form>'
-#				printable = " <a href='modify_user/" + inverted + "'>" + printable + '</a>'
+				if len(result) == 2:
+					printable = " <a href='modify_user/" + inverted + "'>" + printable + '</a>'
+				else:
+					printable += " <a href='modify_user/" + inverted + "'>Modify" '</a><form action="/delete_user" method="POST">\n	<input type="hidden" name="user" value="' + inverted + '">\n    <input type="image" src="https://cdn-icons-png.flaticon.com/512/58/58326.png" alt="submit" width="20" height="20">\n</form>'
 
 			if len(result) == 2:
 				response += "<ul><li>" + printable
 				tags.insert(0, "</ul>")
 				tags.insert(0, "</li>")
+
 			elif len(result) > last:
+				parents.insert(0, last_item)
 				response += "<ul><li>" + printable
 				tags.insert(0, "</ul>")
 				tags.insert(0, "</li>")
+
 			elif len(result) == last:
 				response += "</li><li>" + printable
+
 			elif len(result) < last:
-				response += "</li><a href='#'>Add object</a></ul></li><li>" + printable
+				response += "</li><a href='/create_object?parent=" + parents[0] + "'>Add child object</a></ul></li><li>" + printable
+				parents.pop(0)
 				tags.remove("</li>")
 				tags.remove("</ul>")
 				tags.remove("</li>")
 				tags.insert(0, "</li>")
+
+			last_item = inverted
 			last = len(result)
 
 		while len(tags) != 0:
 			if len(tags) > 1:
 				if str(tags[0]) == "</ul>":
-					insert = "<li><a href='#'>Add object</a></li>" + tags[0]
+					insert = "<li><a href='/create_object?parent=" + parents[0] + "'>Add child object</a></li>" + tags[0]
+					parents.pop(0)
 				else:
 					insert = tags[0]
 				response += insert + "\n"
