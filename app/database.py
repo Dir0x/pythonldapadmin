cc94737ceb34622e08ca715b795f580243c5e18d
Daniel <daniel@debian>
2022-05-26 18:51:42 +0200

    Minor fixes

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ import sqlite3, os
 db_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../app.db')
 connection = sqlite3.connect(db_path)
 cursor = connection.cursor()
-print("hola")
+
 cursor.execute('''create table if not exists events (id integer primary key autoincrement, timestamp text, type text, do_user text)''')
 cursor.execute('''create table if not exists add_object (id integer primary key autoincrement, full_dn text, ldif text, type text, event_id integer, foreign key(event_id) references events(id))''')
 cursor.execute('''create table if not exists move_object (id integer primary key autoincrement, original text, cn text, destination text, event_id integer, foreign key(event_id) references events(id))''')
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
 class Config(object):
-	SECRET_KEY = '5aBniNA&NR#b1a6gLnl!!L0j8GXm.A#CP1t4ccWpTq@8cureeB'
+	SECRET_KEY = os.environ.get('SECRET_KEY') or '5aBniNA&NR#b1a6gLnl!!L0j8GXm.A#CP1t4ccWpTq@8cureeB'
